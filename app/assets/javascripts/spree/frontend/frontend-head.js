80665de2cd3949268fc2eb601f0ac439a485ea02
fredian <fredian.pc@gmail.com>
2020-05-09 19:33:02 +0700

    fix link url

--- app/assets/javascripts/spree/frontend/frontend-head.js
+++ app/assets/javascripts/spree/frontend/frontend-head.js
@@ -1,1 +1,13 @@
+//= require jquery3
+//= require jquery_ujs
+//= require popper
+//= require bootstrap
+//= require jquery.payment
+//= require spree
+//= require polyfill.min
+//= require fetch.umd
+//= require spree/api/main
+//= require lazysizes.min
+//= require turbolinks
+
 //= require ./template/frontend-head
--- app/assets/javascripts/spree/frontend/template/application.js
+++ app/assets/javascripts/spree/frontend/template/application.js
@@ -5,6 +5,7 @@
 //= require ./vendors/jquery.easy-pie-chart/jquery.easypiechart.min
 //= require ./vendors/jquery.gmap/jquery.gmap.min
 //= require ./vendors/jquery.lazyload/jquery.lazyload.min
+
 //= require ./vendors/isotope/jquery.isotope.min
 //= require ./vendors/owl.carousel/owl.carousel.min
 //= require ./vendors/magnific-popup/jquery.magnific-popup.min
@@ -12,9 +13,6 @@
 //= require ./vendors/vivus/vivus.min
 
 //= require ./js/theme
-
-//= require ./vendors/rs-plugin/js/jquery.themepunch.tools.min
-//= require ./vendors/rs-plugin/js/jquery.themepunch.revolution.min
 
 //= require ./js/custom
 
