cab1821fe3eb872b5b10651de5b2b14d789d1aaa
fredian <fredian.pc@gmail.com>
2020-06-03 23:10:30 +0700

    fix aws

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -28,25 +28,18 @@ Spree.config do |config|
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
         bucket:            ENV['S3_BUCKET_NAME']
       },
-  
+    
       storage:        :s3,
-      s3_headers:     { 'Cache-Control' => 'max-age=31557600' },
-      s3_protocol:    'https',
+      s3_region:      ENV['AWS_S3_REGION'],
+      s3_headers:     { "Cache-Control" => "max-age=31557600" },
+      s3_protocol:    "https",
       bucket:         ENV['S3_BUCKET_NAME'],
-      url:            ':s3_domain_url',
-  
-      styles: {
-        mini:     '48x48>',
-        small:    '100x100>',
-        product:  '240x240>',
-        large:    '600x600>'
-      },
-  
-      path:           '/:class/:id/:style/:basename.:extension',
-      default_url:    'noimage/:style.png',
-      default_style:  'product'
+      url:            ":s3_domain_url",
+    
+      path:           "/:class/:id/:style/:basename.:extension",
+      default_url:    "/:class/:id/:style/:basename.:extension",
     }
-  
+    
     attachment_config.each do |key, value|
       Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
     end
